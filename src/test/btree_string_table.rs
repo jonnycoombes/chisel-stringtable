f058cf1d08373aa7e535223aac365a0a618bdd42
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-03-18 08:11:07 +0000

    * Basic implementation and tests completed

--- src/test/btree_string_table.rs
+++ src/test/btree_string_table.rs
@@ -1,23 +1,57 @@
-use std::{env};
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
 use crate::btree_string_table::BTreeStringTable;
 use crate::common::StringTable;
 
-fn all_words_table() -> BTreeStringTable<'static> {
+fn load_words() -> BTreeStringTable<'static> {
+    let mut table = BTreeStringTable::new();
     let path = env::current_dir()
         .unwrap()
         .join("src/test/fixtures/words");
     let words = BufReader::new(File::open(path).unwrap()).lines();
-    let mut table = BTreeStringTable::new();
-    for word in words.flatten(){
+    for word in words.flatten() {
         table.add(word.trim());
     }
     table
 }
 
 #[test]
-fn should_perform_lookups_correctly() {
-   let words  = all_words_table();
+fn should_perform_positive_lookups_correctly() {
+    let words = load_words();
+    assert!(words.contains("abada"));
+    assert!(words.contains("alphabet"));
+    assert!(words.contains("abwab"));
+    assert!(words.contains("buzzed"));
+    assert!(words.contains("civilizee"));
+    assert!(words.contains("jitter"));
+    assert!(words.contains("mastras"));
+    assert!(words.contains("neotype"));
+    assert!(words.contains("prayer-granting"));
+    assert!(words.contains("sloted"));
+    assert!(words.contains("truthy"));
+    assert!(words.contains("xyphoid"));
+    assert!(words.contains("zymurgy"));
+}
 
+#[test]
+fn should_perform_negative_lookups_correctly() {
+    let words = load_words();
+    assert!(!words.contains("zjfgkdf"));
+    assert!(!words.contains("l.s.f.s.w.r.t"));
+    assert!(!words.contains("erersdfsv"));
+    assert!(!words.contains("comcomcomcocmcoasdfasd"));
+    assert!(!words.contains("asdfaworofvf"));
+    assert!(!words.contains("gibberbiffereidsfsdfs"));
+}
+
+#[test]
+fn should_return_a_cow() {
+    let words = load_words();
+    let hash = fxhash::hash32("alphabet");
+    let cow = words.get(hash);
+    assert!(&cow.is_some());
+    let value = String::from(format!("{}", cow.unwrap()));
+    assert_eq!(value, "alphabet")
 }
